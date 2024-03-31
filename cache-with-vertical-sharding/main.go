4a07151e21e9e3bac4a589c216e6bfaa717c79b2
reaper8055 <11490705+reaper8055@users.noreply.github.com>
2024-04-01 00:51:42 +0530

    added main.go to cache-with-vertical-sharding

---
+++ cache-with-vertical-sharding/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/reaper8055/distributed-cache/cache-with-vertical-sharding/cache"
+)
+
+func main() {
+	runCacheFunc := func() {
+		cache := cache.New(1)
+		cache.Set("a", 1)
+		cache.Set("b", 2)
+		cache.Set("c", 3)
+
+		keys := cache.Keys()
+		for _, key := range keys {
+			fmt.Println(key)
+			fmt.Println(cache.Get(key))
+		}
+	}
+
+	runCacheFunc()
+}
